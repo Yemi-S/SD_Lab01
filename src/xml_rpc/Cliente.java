f8c2a4e033c8bfc15f96e3fa1037f0c5616f9737
Yemi-S <zpfmgy@hotmail.com>
2015-11-06 06:02:39 -0500

    XML_RPC, definición de clases

---
+++ src/xml_rpc/Cliente.java
@@ -0,0 +1,26 @@
+
+package xml_rpc;
+
+import java.util.*;
+import org.apache.xmlrpc.*;
+
+public class Cliente {
+   public static void main (String [] args) {
+   
+      try {
+         XmlRpcClient server = new XmlRpcClient("http://localhost:8086/RPC2"); 
+         Vector params = new Vector();
+         
+         params.addElement(new Integer(17));
+         params.addElement(new Integer(13));
+
+         Object result = server.execute("sample.sum", params);
+
+         int sum = ((Integer) result).intValue();
+         System.out.println("The sum is: "+ sum);
+
+      } catch (Exception exception) {
+         System.err.println("JavaClient: " + exception);
+      }
+   }
+}
